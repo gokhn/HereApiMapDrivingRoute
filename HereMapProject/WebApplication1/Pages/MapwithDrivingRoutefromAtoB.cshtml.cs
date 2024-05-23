2e66fbfe304086a01964eca61d12ac166bbe677b
Gokhan Gungor <gokhngungor@gmail.com>
2024-05-23 08:50:21 +0300

    Haritaya Marker ekleme

---
+++ HereMapProject/WebApplication1/Pages/MapwithDrivingRoutefromAtoB.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1.Pages
+{
+    public class MapwithDrivingRoutefromAtoBModel : PageModel
+    {
+        private readonly ILogger<MapwithDrivingRoutefromAtoBModel> _logger;
+
+        public MapwithDrivingRoutefromAtoBModel(ILogger<MapwithDrivingRoutefromAtoBModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
